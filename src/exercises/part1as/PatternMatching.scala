12c375f6d68ed5a29c7443fdc6b528aaf6519317
Gabriel Gonzalez <gabokappa@gmail.com>
2020-04-20 10:02:28 +0100

    Lesson 3 exercie in pattern matching

---
+++ src/exercises/part1as/PatternMatching.scala
@@ -0,0 +1,30 @@
+package exercises.part1as
+
+object PatternMatching extends App {
+
+  // usually when you create singleton objects for unapply methods for conditions these objects are named in lower case
+
+  object even {
+    def unapply(arg: Int): Option[Boolean] =
+      if (arg % 2 == 0) Some(true)
+      else None
+  }
+
+  object singleDigit {
+    def unapply(arg: Int): Boolean = arg < 10 && arg > -10
+  }
+
+  val n: Int = 8
+  val mathProperty = n match {
+    case singleDigit() => "single digit"
+    case even(_) => "an even number"
+    case _ => "no property"
+  }
+
+  println(mathProperty)
+
+  // in terms of style note how singleDigit doesn't use the option just uses a boolean
+  // quick way to write tests is by writing singleton objects with unapply with a boolean
+  // although this might get a bit verbose with multiple objects
+
+}
--- src/lectures/part1as/AdvancedPatternMatching.scala
+++ src/lectures/part1as/AdvancedPatternMatching.scala
@@ -19,7 +19,13 @@ object AdvancedPatternMatching extends App {
 
   // define a companion
   object Person {
-    def unapply(person: Person): Option[(String, Int)] = Some((person.name, person.age))
+    def unapply(person: Person): Option[(String, Int)] =
+      if (person.age < 21) None
+      else Some((person.name, person.age))
+
+     def unapply(age: Int): Option[String] =
+       Some(if (age < 21) "minor" else "major")
+
   }
 
   val bob = new Person("Bob", 25)
@@ -27,13 +33,43 @@ object AdvancedPatternMatching extends App {
     case Person(n, a) => s"Hi, my name is $n and I'm $a years old"
   }
 
+  println(greeting)
+
   /* Although Person not a case class because we created a companion object with a method that returns a Option(String, Int) we can pattern match
   because of the unapply method.
 
-  When the runtime happens it goes through different steps. As it reads the match case Person(n, a) it looks for a method unapply in an object called person
+  When the runtime happens it goes through different steps. As it reads the match case Person(n, a) it looks for a method unapply in an object called person.
+  If unapply returns something else this would work for example if unapply was
 
+  object Person {
+    def unapply(person: Person): Option[(String, Int)] =
+    if (person.age < 26) None
+    else Some((person.name, person.age))
+  }
+
+  The above will cause a pattern match error. The class doesn't even have to have the same name as the object, as long as the pattern match is done against the objects
+  name.
    */
 
+
+  val legalStatus = bob.age match {
+    case Person(status) => s"Legal age is $status"
+  }
+
+  // the above works as a pattern match because bob.age matches the other unapply method in the Person object that takes Int as an argument and it returns a Some with a string
+
+  println(legalStatus)
+
+  /*
+  Exercise.
+   */
+
+  val n: Int = 45
+  val mathProperty = n match {
+    case x if x < 10 => "single digit"
+    case x if x % 2 == 0 => "an even number"
+    case _ => "no property"
+  }
 
 
 
