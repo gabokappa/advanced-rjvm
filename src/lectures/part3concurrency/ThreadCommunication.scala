6fc2a4dc3bbc80cfb0dda9df6488d8a5bb1c6959
Gabriel Gonzalez <gabokappa@gmail.com>
2020-07-20 09:38:08 +0100

    multiple producers and multiple consuemrs

--- src/lectures/part3concurrency/ThreadCommunication.scala
+++ src/lectures/part3concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.part3concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication extends App {
 
   /*
@@ -61,12 +64,12 @@ object ThreadCommunication extends App {
     val container = new SimpleContainer
 
     val consumer = new Thread(() => {
-      println("[consumer waiting...")
+      println("[consumer] waiting...")
       container.synchronized { // locks it in
         container.wait() // this makes it waiting, the producer thread will release the lock
       }
-      // at this point the container must have a value, the only person that can wake up from this calue is the producer
-      println("[consumer] I have consumer " + container.get)
+      // at this point the container must have a value, the only person that can wake up the consumer from this value is the producer
+      println("[consumer] I have consumed " + container.get)
     })
 
     val producer = new Thread(() => {
@@ -75,7 +78,7 @@ object ThreadCommunication extends App {
       val value = 42
 
       container.synchronized {
-        println("producer producing" + value)
+        println("[producer] produced " + value)
         container.set(value)
         container.notify()
       }
@@ -85,6 +88,124 @@ object ThreadCommunication extends App {
 
   }
 
-  smartProdCons()
+  // smartProdCons()
+
+  /*
+  producer -> [ ?, ?, ?] - > consumer
+  Both the producer and consumer may block eachother.
+   */
+
+  def prodConsLargeBuffer(): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    val consumer = new Thread(() => {
+      val random = new Random()
+
+      while(true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            println("[consumer] buffer empty, waiting...")
+            buffer.wait()
+          }
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue()
+          println("[consumer] consumed " + x)
+
+          buffer.notify() // this wakes up the producer thread to wakeup as a value has been consumed
+        }
+        Thread.sleep(random.nextInt(250))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random()
+      var i = 0
+
+      while(true) {
+        buffer.synchronized {
+          if (buffer.size == capacity) {
+            println("producer buffer is full, waiting...")
+            buffer.wait() // waiting for the consumer to wake me up
+          }
+          // there must be at least ONE empty space in the buffer
+          println("producer producing " + i)
+          buffer.enqueue(i)
+
+          buffer.notify() // this tells the consumer thread to wake up. as now it has added a value
+
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(250))
+      }
+    })
+    consumer.start()
+    producer.start()
+  }
+
+//  prodConsLargeBuffer()
+
+  /*
+  prod-cons level 3. Have a limited capacity buffer, but have multiples producers and multiple consumers acting on the same buffer.
+  producer1 -> [ ? ? ? ?] -> consumer1
+  producer2 -> --^    ^___ consumer2
+   */
+
+  class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+
+      while(true) {
+        buffer.synchronized {
+          while (buffer.isEmpty) {
+            println(s"[consumer $id] buffer empty, waiting...")
+            buffer.wait()
+          }
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue()
+          println(s"[consumer $id] consumed " + x)
+
+          buffer.notify() // this wakes up either a Cosumer thread or porducer thread
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          while (buffer.size == capacity) {
+            println(s"producer$id buffer is full, waiting...")
+            buffer.wait() // waiting for the consumer to wake me up
+          }
+          // there must be at least ONE empty space in the buffer
+          println(s"producer$id producing " + i)
+          buffer.enqueue(i)
+
+          buffer.notify() // this tells the consumer or producer thread to wake up
+
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+    (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+  }
+
+  multiProdCons(3, 3)
 
 }
