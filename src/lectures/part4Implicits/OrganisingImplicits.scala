1d95ec658f1420d83fc8c44d922a9857623b57a1
Gabriel Gonzalez <gabokappa@gmail.com>
2020-07-28 15:07:39 +0100

    Organizing implicits finished

--- src/lectures/part4Implicits/OrganisingImplicits.scala
+++ src/lectures/part4Implicits/OrganisingImplicits.scala
@@ -25,10 +25,14 @@ object OrganisingImplicits extends App {
     Person("Amy", 22),
     Person("John", 66)
   )
-object Person {
-  implicit val alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0) // how to compare strings
-}
-  println(persons.sorted)
+//object Person {
+//  implicit val alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0) // how to compare strings
+//}
+
+  // implicit val ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age) // this now takes precedent over the implicit in the Person object
+  // there are now multiple possible values for the implicit as we have one in Person and one here.
+
+  // println(persons.sorted)
 
 /*
 Implicit scope (priority in this list is top to bottom)
@@ -43,6 +47,51 @@ Implicit scope (priority in this list is top to bottom)
 
   // def sorted[B >: A](implicit ord: Ordering[B]): List[B] This is what the sorting does in the example above which for our case is a List
 
+  /* BEST PRACTICE
+  - When you want to define an implicit val
+
+  #1 If there is a single possible value for it and you can edit the code for the type then define the implicit in the companion object.
+
+  #2 If there are many possible values for it, but a single "good" one (that make sense for most of the cases) then put the "good" one in the companion object and the other implicits in the local scope or other objects
+     if there are many we should package them up separately, and the user should explicitly import the right container.
+   */
+
+  // PACKAGING THE IMPLICITS IN SEPARATE OBJECTS
+
+  object AlphabeticNameOrdering {
+    implicit val alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0) // how to compare strings
+  }
+
+  object AgeOrdering {
+    implicit val ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age)
+  }
+
+  // when you want to use one of them you can import them with an underscore or .valueName to be specific
+
+  import AgeOrdering._
+  println(persons.sorted)
+
+  /*
+  Exercise
+  1) Add three orderings by different criteria
+  - Total price (most used 50% used)
+  - by unit count = 25% used
+  - by unit price = 25% used
+   */
+
+  case class Purchase(nUnits: Int, unitPrice: Double)
+
+  object Purchase {
+    implicit val totalPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan((a, b) => (a.nUnits * a.unitPrice) < (b.nUnits * b.unitPrice))
+  }
+
+  object UnitCount {
+    implicit val unitCountOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.nUnits < _.nUnits)
+  }
+ // these can also be declared in the local scope
+  object UnitPrice {
+    implicit val unitPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.unitPrice < _.unitPrice)
+  }
 
 
 }
